84b0dd90a285b383cf1aae4f0a4ff19d3bb8dc36
Christian.Zwiessler <christian.zwiessler@gmail.com>
2024-04-26 13:25:08 +0200

    implemented working swin_base_patch4_window12_384 model from timm

--- src/model.py
+++ src/model.py
@@ -18,38 +18,14 @@ def init_model(model_name, num_classes):
         model_path = "C:/Users/christian.zwiessler/PycharmProjects/timmpy36/src/lightning/model_architectures/coat_model"
         config = AutoConfig.from_pretrained(model_path)
         model = AutoModel.from_config(config)
-
-        #model = timm.create_model(model_name, pretrained=False)
         num_features = model.head.fc.in_features
         model.head.fc = torch.nn.Linear(num_features, num_classes)
 
-    elif model_name == 'resnet18':
-        model = timm.create_model(model_name, pretrained=True)
-        num_features = model.fc.in_features
-        model.fc = torch.nn.Linear(num_features, num_classes)
-
-
-    elif model_name == 'timm/maxvit_xlarge_tf_512.in21k_ft_in1k':
-        model = timm.create_model(model_name, pretrained=True)
-        num_features = model.head.fc.in_features
+    elif model_name == 'swin_base_patch4_window12_384':
+        model = timm.create_model(model_name)
+        num_features = model.head.in_features
         model.head.fc = torch.nn.Linear(num_features, num_classes)
 
-    elif model_name == 'timm/maxvit_xlarge_tf_384.in21k_ft_in1k':
-        model = timm.create_model(model_name, pretrained=True)
-        num_features = model.head.fc.in_features
-        model.head.fc = torch.nn.Linear(num_features, num_classes)
-
-    elif model_name == 'timm/maxvit_base_tf_384.in21k_ft_in1k':
-        model = timm.create_model(model_name, pretrained=True)
-        num_features = model.head.fc.in_features
-        model.head.fc = torch.nn.Linear(num_features, num_classes)
-
-    elif model_name == 'timm/davit_base.msft_in1k':
-        model = timm.create_model(model_name, pretrained=False)
-        data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lightning', 'model_architectures')
-        model.load_state_dict(torch.load(data_path + '/model_davit_base_weights.bin'))
-        num_features = model.head.fc.in_features
-        model.head.fc = torch.nn.Linear(num_features, num_classes)
 
     else:
         raise ValueError(f"Model {model_name} not supported.")
